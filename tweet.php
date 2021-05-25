eb263a954db09192f03f0cec6dde5534752fffde
simibot <84782994+simibot@users.noreply.github.com>
2021-05-25 21:56:31 +0700

    Update tweet.php

--- tweet.php
+++ tweet.php
@@ -24,10 +24,10 @@ $data = explode("/", $s);
 // Get API key & secret from https://developer.twitter.com
 // Make sure your APP Token set "write" permissions
 
-$consumer_key        = '';
-$consumer_secret     = '';
-$access_token        = '';
-$access_token_secret = '';
+$consumer_key        = '6HEM5SGXF9oROs6Pun1oV3Zjw';
+$consumer_secret     = '8700IrszDkN1lLIHwEPGjgOpyIkoT6eey2dnc7OUYqrUbSUN4S';
+$access_token        = '1395423506557390853-eUjthnOmykXrV4YStAkYlDCw7Krdih';
+$access_token_secret = 'hukdZK1WNiK39ZvFyOjKTGIn4h4MLUA7sgy4yXsB7q3AO';
 
 $conn = new TwitterOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
 $conn->get('account/verify_credentials');
